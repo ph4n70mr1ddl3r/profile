2aadd12252a32f642261f2d956a2d30f72b5725f
ph4n70mr1ddl3r <105593742+ph4n70mr1ddl3r@users.noreply.github.com>
2025-12-11 23:06:34 +0800

    clear

--- src/lib/telemetry/request-context.ts
+++
@@ -1,7 +0,0 @@
-import type { NextRequest } from 'next/server';
-
-export const getRequestContext = (req: NextRequest) => {
-  const requestId = req.headers.get('x-request-id') ?? undefined;
-  const tenantId = req.headers.get('x-tenant-id') ?? undefined;
-  return { requestId, tenantId };
-};
